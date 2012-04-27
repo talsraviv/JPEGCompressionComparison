7cd539da41aadd1b03834dd573e511517636ab29
Tal Raviv <traviv@ecquire.com>
2012-04-27 20:43:32 +0300

    Clear up the contentscript

--- contentscript.js
+++ contentscript.js
@@ -4,20 +4,23 @@ var log = function(a){console.log(a)};
 chrome.extension.onRequest.addListener(
     function(request, sender, sendResponse) {
         if (request.state.length){
+            //This is what the background gave us
             var original_vs_jpegmini = JSON.parse(request.state);
-            //TODO: Process the state object and tell the user the size difference
-            //REMEMBER THAT THE ORIGINAL AND JPEGMINI OBJECTS MAY NOT TOTALLY MATCH
-            //SO ONLY CALCULATE URLS THEY HAVE IN COMMON
+
             var total_original_bytes = 0;
             var total_jpeg_mini_bytes = 0;
+
+            //Calculate totals
             for(var original_url in original_vs_jpegmini['original']){
-                //IF we have a url appearing in both objects
+                //IF we have a url appearing in both objects that we can compare
                 if (original_url in original_vs_jpegmini['jpegmini']){
+                    //ADD IT TO BOTH SUMS
                     total_original_bytes += original_vs_jpegmini['original'][original_url]
                     total_jpeg_mini_bytes += original_vs_jpegmini['jpegmini'][original_url]
                 }
             }
             log(original_vs_jpegmini);
+
             alert('The difference in sizes is...' + total_original_bytes + ' vs ' + total_jpeg_mini_bytes);
         }
     }
